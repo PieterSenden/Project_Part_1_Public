36ff6d2f90a992cadcd400a6518448d90e17000e
JorisCe <JorisCe@users.noreply.github.com>
2017-03-11 19:47:46 +0100

    Documentatie toegevoegd

--- IllegalCoordinateException.java
+++ IllegalCoordinateException.java
@@ -1,5 +1,11 @@
 package asteroids.model;
 
+/**
+ * A class for signaling exceptions in coordinates of a position.
+ * 
+ * @author Joris Ceulemans & Pieter Senden
+ * @version 1.0
+ */
 public class IllegalCoordinateException extends RuntimeException {
 	
 	public IllegalCoordinateException() {
