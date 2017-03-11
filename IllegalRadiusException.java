7ac117710d5b90a75aae971cbc92a67b312ff97f
JorisCe <JorisCe@users.noreply.github.com>
2017-03-11 19:48:45 +0100

    Achternamen toegevoegd

--- IllegalRadiusException.java
+++ IllegalRadiusException.java
@@ -3,7 +3,7 @@ package asteroids.model;
 /**
  * A class for signaling exceptions in positions.
  * 
- * @author Joris & Pieter
+ * @author Joris Ceulemans & Pieter Senden
  * @version 1.0
  */
 public class IllegalRadiusException extends RuntimeException {
@@ -15,5 +15,5 @@ public class IllegalRadiusException extends RuntimeException {
 	}
 	
 	
-	private static final long serialVersionUID = -8392006934330596047L;
+	private static final long serialVersionUID = 1L;
 }
